706d79f8b2756c1f8ad8bc665c8a33009afb0376
Stark <anfernoangel@gmail.com>
2021-02-14 11:55:00 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace AlarmClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Hours = new System.Windows.Forms.NumericUpDown();
+            this.Minutes = new System.Windows.Forms.NumericUpDown();
+            this.Seconds = new System.Windows.Forms.NumericUpDown();
+            this.turnOnBtn = new System.Windows.Forms.Button();
+            this.turnOffBtn = new System.Windows.Forms.Button();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Seconds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Magenta;
+            this.label1.Location = new System.Drawing.Point(162, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "00:00:00";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Hours
+            // 
+            this.Hours.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Hours.ForeColor = System.Drawing.Color.Blue;
+            this.Hours.Location = new System.Drawing.Point(169, 54);
+            this.Hours.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.Hours.Name = "Hours";
+            this.Hours.Size = new System.Drawing.Size(46, 31);
+            this.Hours.TabIndex = 2;
+            this.Hours.Value = new decimal(new int[] {
+            22,
+            0,
+            0,
+            0});
+            // 
+            // Minutes
+            // 
+            this.Minutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Minutes.ForeColor = System.Drawing.Color.Blue;
+            this.Minutes.Location = new System.Drawing.Point(221, 54);
+            this.Minutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.Minutes.Name = "Minutes";
+            this.Minutes.Size = new System.Drawing.Size(44, 31);
+            this.Minutes.TabIndex = 3;
+            this.Minutes.Value = new decimal(new int[] {
+            22,
+            0,
+            0,
+            0});
+            // 
+            // Seconds
+            // 
+            this.Seconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Seconds.ForeColor = System.Drawing.Color.Blue;
+            this.Seconds.Location = new System.Drawing.Point(271, 54);
+            this.Seconds.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.Seconds.Name = "Seconds";
+            this.Seconds.Size = new System.Drawing.Size(44, 31);
+            this.Seconds.TabIndex = 4;
+            this.Seconds.Value = new decimal(new int[] {
+            22,
+            0,
+            0,
+            0});
+            // 
+            // turnOnBtn
+            // 
+            this.turnOnBtn.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.turnOnBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.turnOnBtn.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.turnOnBtn.ForeColor = System.Drawing.Color.Yellow;
+            this.turnOnBtn.Location = new System.Drawing.Point(165, 102);
+            this.turnOnBtn.Name = "turnOnBtn";
+            this.turnOnBtn.Size = new System.Drawing.Size(75, 23);
+            this.turnOnBtn.TabIndex = 5;
+            this.turnOnBtn.Text = "Включить";
+            this.turnOnBtn.UseVisualStyleBackColor = false;
+            this.turnOnBtn.Click += new System.EventHandler(this.turnOnBtn_Click);
+            // 
+            // turnOffBtn
+            // 
+            this.turnOffBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.turnOffBtn.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.turnOffBtn.ForeColor = System.Drawing.Color.Red;
+            this.turnOffBtn.Location = new System.Drawing.Point(241, 102);
+            this.turnOffBtn.Name = "turnOffBtn";
+            this.turnOffBtn.Size = new System.Drawing.Size(85, 23);
+            this.turnOffBtn.TabIndex = 6;
+            this.turnOffBtn.Text = "Выключить";
+            this.turnOffBtn.UseVisualStyleBackColor = true;
+            this.turnOffBtn.Click += new System.EventHandler(this.turnOffBtn_Click);
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(0, 140);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(318, 31);
+            this.mediaPlayer.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(318, 171);
+            this.Controls.Add(this.mediaPlayer);
+            this.Controls.Add(this.turnOffBtn);
+            this.Controls.Add(this.turnOnBtn);
+            this.Controls.Add(this.Seconds);
+            this.Controls.Add(this.Minutes);
+            this.Controls.Add(this.Hours);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Часы-будильник";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Seconds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown Hours;
+        private System.Windows.Forms.NumericUpDown Minutes;
+        private System.Windows.Forms.NumericUpDown Seconds;
+        private System.Windows.Forms.Button turnOnBtn;
+        private System.Windows.Forms.Button turnOffBtn;
+        private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClock
+{
+    public partial class Form1 : Form
+    {
+        private int H;
+        private int M;
+        private int S;
+
+        private int h;
+        private int m;
+        private int s;
+        private DateTime currentTime;
+        public Form1()
+        {
+            InitializeComponent();
+            turnOffBtn.Enabled = false;
+        }
+
+        private void DisplayTime()
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayTime();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DisplayTime();
+            currentTime = DateTime.Now;
+            h = currentTime.Hour;
+            m = currentTime.Minute;
+            s = currentTime.Second;
+            if (h == H && m == M && s == S)
+            {
+                string soundPath = Path.GetFullPath(@"..\..\Sounds\Sound.mp3");
+                mediaPlayer.URL = soundPath;
+                mediaPlayer.Ctlcontrols.play();
+                //MessageBox.Show("Время истекло", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                FinishCount();
+                mediaPlayer.Ctlcontrols.stop();
+            }
+        }
+
+        private void turnOnBtn_Click(object sender, EventArgs e)
+        {
+            H = (int) Hours.Value;
+            M = (int) Minutes.Value;
+            S = (int) Seconds.Value;
+            MessageBox.Show("Будильник включен", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            turnOnBtn.Enabled = false;
+            turnOffBtn.Enabled = true;
+        }
+
+        private void FinishCount()
+        {
+            H = M = S = 0;
+            Hours.Value = 0;
+            Minutes.Value = 0;
+            Seconds.Value = 0;
+            MessageBox.Show("Будильник выключен", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            turnOnBtn.Enabled = true;
+            turnOffBtn.Enabled = false;
+        }
+        private void turnOffBtn_Click(object sender, EventArgs e)
+        {
+            FinishCount();
+        }
+    }
+}
